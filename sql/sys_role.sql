e53bee39ea179c241cc4f3eca3c4f2314605f375
wangzenghui123 <1573713057@qq.com>
2025-05-26 16:33:24 +0800

    5.26

---
+++ sql/sys_role.sql
@@ -0,0 +1,1 @@
+INSERT INTO `sys_role` (`id`, `role_name`) VALUES ('1', 'superadmin');
--- sql/syspermission.sql
+++ sql/syspermission.sql
@@ -1,62 +1,27 @@
-/*
- Navicat Premium Dump SQL
-
- Source Server         : demo
- Source Server Type    : MySQL
- Source Server Version : 80040 (8.0.40)
- Source Host           : localhost:3306
- Source Schema         : study
-
- Target Server Type    : MySQL
- Target Server Version : 80040 (8.0.40)
- File Encoding         : 65001
-
- Date: 21/05/2025 17:37:59
-*/
-
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for syspermission
--- ----------------------------
-DROP TABLE IF EXISTS `syspermission`;
-CREATE TABLE `syspermission`  (
-  `id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-  `code` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `perms` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `method` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `pid` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `orderNum` int NULL DEFAULT NULL,
-  `type` int NULL DEFAULT NULL,
-  `status` int NULL DEFAULT NULL,
-  `createTime` datetime NULL DEFAULT NULL,
-  `updateTime` datetime NULL DEFAULT NULL,
-  `deleted` int NULL DEFAULT NULL,
-  `pidName` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of syspermission
--- ----------------------------
-INSERT INTO `syspermission` VALUES ('1', '', '系统管理', NULL, NULL, NULL, '0', 2, 1, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '根目录');
-INSERT INTO `syspermission` VALUES ('10', NULL, '用户查询', NULL, NULL, NULL, '4', 1, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
-INSERT INTO `syspermission` VALUES ('11', NULL, '用户新增', NULL, NULL, NULL, '4', 2, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
-INSERT INTO `syspermission` VALUES ('12', NULL, '用户修改', NULL, NULL, NULL, '4', 3, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
-INSERT INTO `syspermission` VALUES ('13', NULL, '用户删除', NULL, NULL, NULL, '4', 4, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
-INSERT INTO `syspermission` VALUES ('14', NULL, '用户导出', NULL, NULL, NULL, '4', 5, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
-INSERT INTO `syspermission` VALUES ('15', NULL, '用户导入', NULL, NULL, NULL, '4', 6, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
-INSERT INTO `syspermission` VALUES ('16', NULL, '重置密码', NULL, NULL, NULL, '4', 7, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
-INSERT INTO `syspermission` VALUES ('2', '', '系统监控', NULL, NULL, NULL, '0', 3, 1, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:55', 0, '根目录');
-INSERT INTO `syspermission` VALUES ('3', '', '系统工具', NULL, NULL, NULL, '0', 4, 1, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '根目录');
-INSERT INTO `syspermission` VALUES ('4', '', '用户管理', NULL, NULL, NULL, '1', 1, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统管理');
-INSERT INTO `syspermission` VALUES ('5', '', '角色管理', NULL, NULL, NULL, '1', 2, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统管理');
-INSERT INTO `syspermission` VALUES ('6', '', '菜单管理', NULL, NULL, NULL, '1', 3, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统管理');
-INSERT INTO `syspermission` VALUES ('7', '', '在线用户', NULL, NULL, NULL, '2', 1, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统监控');
-INSERT INTO `syspermission` VALUES ('8', '', '表单构成', NULL, NULL, NULL, '3', 1, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统工具');
-INSERT INTO `syspermission` VALUES ('9', '', '首页', NULL, NULL, NULL, '0', 1, 1, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '根目录');
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('1', NULL, '系统管理', NULL, NULL, NULL, '0', 2, 1, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '根目录');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('10', NULL, '用户查询', 'sys:user:query', NULL, NULL, '4', 1, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('11', NULL, '用户新增', 'sys:user:add', NULL, NULL, '4', 2, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('12', NULL, '用户修改', 'sys:user:edit', NULL, NULL, '4', 3, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('13', NULL, '用户删除', 'sys:user:delete', NULL, NULL, '4', 4, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('14', NULL, '用户导出', 'sys:user:export', NULL, NULL, '4', 5, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('15', NULL, '用户导入', 'sys:user:import', NULL, NULL, '4', 6, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '用户管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('17', NULL, '角色查询', 'sys:role:query', NULL, NULL, '5', 1, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '角色管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('18', NULL, '角色新增', 'sys:role:add', NULL, NULL, '5', 2, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '角色管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('19', NULL, '角色修改', 'sys:role:edit', NULL, NULL, '5', 3, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '角色管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('2', NULL, '系统监控', NULL, NULL, NULL, '0', 3, 1, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:55', 0, '根目录');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('20', NULL, '角色删除', 'sys:role:delete', NULL, NULL, '5', 4, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '角色管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('21', NULL, '角色导出', 'sys:role:export', NULL, NULL, '5', 5, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '角色管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('22', NULL, '角色导入', 'sys:role:import', NULL, NULL, '5', 6, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '角色管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('23', NULL, '菜单查询', 'sys:perm:query', NULL, NULL, '6', 1, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '菜单管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('24', NULL, '菜单新增', 'sys:perm:add', NULL, NULL, '6', 2, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '菜单管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('25', NULL, '菜单修改', 'sys:perm:edit', NULL, NULL, '6', 3, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '菜单管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('26', NULL, '菜单删除', 'sys:perm:delete', NULL, NULL, '6', 4, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '菜单管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('27', NULL, '菜单导出', 'sys:perm:export', NULL, NULL, '6', 5, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '菜单管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('28', NULL, '菜单导入', 'sys:perm:import', NULL, NULL, '6', 6, 3, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '菜单管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('3', NULL, '系统工具', NULL, NULL, NULL, '0', 4, 1, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '根目录');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('4', NULL, '用户管理', 'sys:user:view', '/api/user', NULL, '1', 1, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('5', NULL, '角色管理', 'sys:role:view', '/api/role', NULL, '1', 2, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('6', NULL, '菜单管理', 'sys:perm:view', '/api/permission', NULL, '1', 3, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统管理');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('7', NULL, '在线用户', NULL, NULL, NULL, '2', 1, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统监控');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('8', NULL, '表单构成', NULL, NULL, NULL, '3', 1, 2, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '系统工具');
+INSERT INTO `syspermission` (`id`, `code`, `name`, `perms`, `url`, `method`, `pid`, `orderNum`, `type`, `status`, `createTime`, `updateTime`, `deleted`, `pidName`) VALUES ('9', NULL, '首页', NULL, '/api/home', NULL, '0', 1, 1, 1, '2025-05-21 17:24:51', '2025-05-21 17:24:51', 0, '根目录');
